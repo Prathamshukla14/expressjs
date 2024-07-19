0b0d706a9e036786b90f3422dba7894815fdef27
Prathamshukla14 <105924481+Prathamshukla14@users.noreply.github.com>
2024-07-20 00:17:20 +0530

    second commit

---
+++ practice_backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app  = express();
+const path = require("path");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("index");
+
+})
+
+app.get("/about/:username/:age", function(req, res){
+    res.send(`welcome, ${req.params.username }`)
+
+})
+
+
+app.listen(3000)
